4a531386cc848669040f6d1dbd10a709ad4b1284
youngguyz <2281300996@qq.com>
2022-06-30 10:07:50 +0800

    feat:修改状态

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<script setup>
+import TodoHeader from './components/TodoHeader.vue'
+import TodoMain from './components/TodoMain.vue'
+import TodoFooter from './components/TodoFooter.vue'
+import { ref } from 'vue';
+// 目标一：APP.vue 渲染(父向子传值：自定义属性)
+
+// 目标二：修改状态
+
+// 定义数据
+const list = ref([
+  {
+    id: 1,
+    name: '吃饭',
+    done: true,
+  },
+  {
+    id: 2,
+    name: '睡觉',
+    done: false,
+  },
+  {
+    id: 3,
+    name: '打豆豆',
+    done: false,
+  },
+])
+
+// 父组件方法 子组件传方法过来 修改 checked 父组件也修改 数据绑定
+const hChangeDone=(id)=>{
+ const todo=list.value.find(item=>item.id===id)
+ todo.done=!todo.done
+}
+</script>
+
+<template>
+  <section class="todoapp">
+    <TodoHeader></TodoHeader>
+    <TodoMain :list="list" @changeDone="hChangeDone"></TodoMain>
+    <TodoFooter></TodoFooter>
+  </section>
+</template>
+
+<style></style>
---
+++ src/components/TodoMain.vue
@@ -0,0 +1,37 @@
+<script setup>
+// 接收父组件值
+defineProps({
+  list:{
+    type:Array,
+    required:true
+  }
+})
+// 子向父传值 自定义方法
+const emit =defineEmits(['changeDone'])
+
+// 定义方法
+const hChange=(id)=>{
+  // console.log('打印');
+  emit('changeDone',id)
+}
+</script>
+
+<template>
+  <section class="main">
+    <input id="toggle-all" class="toggle-all" type="checkbox" />
+    <label for="toggle-all">Mark all as complete</label>
+    <ul class="todo-list">
+      <li v-for="item in list" :key="item.id" :class="{completed:item.done}">
+        <div class="view">
+          <input class="toggle" type="checkbox" @change="hChange(item.id)" :checked="item.done" />
+          <label>{{item.name}}</label>
+          <button class="destroy"></button>
+        </div>
+        <input class="edit" value="Create a TodoMVC template" />
+      </li>
+      
+    </ul>
+  </section>
+</template>
+
+<style lang="less" scoped></style>
